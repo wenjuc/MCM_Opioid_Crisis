aced4405b59de549353f391208307ede9e5075c6
doomibox <huweikai@cs.washington.edu>
2019-01-26 18:20:41 -0800

    add mapplot

--- p1.py
+++ p1.py
@@ -1,17 +1,31 @@
+import sys
 import pandas as pd
 from geopy import geocoders  
 gn = geocoders.GeoNames(username='doomibox')
 
-print "reading excel"
-df = pd.ExcelFile("2018_MCMProblemC_DATA/MCM_NFLIS_Data.xlsx").parse(1)
-print list(df.columns.values)
+print "reading data"
+df = pd.read_csv("2018_MCMProblemC_DATA/updated_nflis_data.csv")
+df = df.assign(Location = lambda x: x.County + ", " + x.State)
 
-print "creating location variables"
-df = df.assign(location = lambda x: x.COUNTY + ", " + x.State)
+loc_list = set(df['Location'])
 
-print df.iloc[0].location, gn.geocode(df.iloc[0].location)
+new_df = pd.DataFrame(columns=['Year', 'Location', 'Substance', 'DrugReports', 'County_Rate', 'change'])
 
+row = 0
+for substance in set(df['SubstanceName']):
+	print substance
+	for location in loc_list:
+		info = df.loc[(df['SubstanceName'] == substance) & (df['Location'] == location)]
+		for year in range(2011, 2018):
+			curr_year = info[info['Year']== year]
+			last_year = info[info['Year']== year - 1]
+			c_val = 0.1 if len(curr_year) == 0 else curr_year['County_Rate'].item()
+			l_val = 0.1 if len(last_year) == 0 else last_year['County_Rate'].item()
+			dr = 0 if len(curr_year) == 0 else curr_year['DrugReports'].item()
+			cr = 0 if len(curr_year) == 0 else curr_year['County_Rate'].item()
+			change = (c_val - l_val) / float(l_val)
+			new_df.loc[row] = [year, location, substance, dr, cr, change]
+			row += 1
 
-
-print "writing to out/out.xlsx"
-df.to_excel("out/out.xlsx")
+print "writing to out/out.csv"
+new_df.to_csv("out/out.csv")
