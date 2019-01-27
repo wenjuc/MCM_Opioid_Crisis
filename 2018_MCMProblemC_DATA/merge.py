69a55f0646825eb40a2d2672bd416640e98af690
doomibox <huweikai@cs.washington.edu>
2019-01-27 01:05:09 -0800

    add merged

--- 2018_MCMProblemC_DATA/merge.py
+++ 2018_MCMProblemC_DATA/merge.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 
 print "reading drug_report"
-nflis = pd.read_excel('MCM_NFLIS_Data.xlsx', sheet_name='Data')
-
-
+nflis = pd.ExcelFile('MCM_NFLIS_Data.xlsx').parse(1)
 
 for i in range(10, 17):
 	filename = "ACS_" + str(i) + "_5YR_DP02"
@@ -13,12 +11,21 @@ for i in range(10, 17):
 	year = int("20" + str(i))
 	filepath = filename + '/' + filename + "_with_ann.csv"
 	geo_info = pd.read_csv(filepath)
-	curr_year = nflis[nflis['YYYY']==year]
+	
+	
+	drop_cols = []
+	for col in list(geo_info.columns):
+		if type(geo_info[col].iloc[0]) == str and len(geo_info[geo_info[col].str.match("\(X\)") | geo_info[col].str.match("\*")]) > 0:
+			drop_cols.append(col)
+	print "dropping n/a cols, count: " + str(len(drop_cols))
+	print drop_cols
+	geo_info.drop(drop_cols, axis=1, inplace=True)
 
+	print "cleaning current year"
+	curr_year = nflis[nflis['YYYY']==year]
 	curr_year = curr_year.groupby(['YYYY', 'FIPS_Combined', 'COUNTY', 'State'], as_index=False, group_keys=False).agg({'TotalDrugReportsCounty': np.sum})
 
 	print "merging"
-	print list(curr_year.columns)
 	df = curr_year.merge(geo_info, left_on="FIPS_Combined", right_on="GEO.id2")
 
 	newpath = "out/" + str(year) + "_merged.csv"
